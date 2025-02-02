c897bec0223dc7adf43a33fdc0894c3454b2c194
Stefan Haglund <stefan.haglund@me.com>
2025-02-02 13:17:47 +0100

    Solution and unit tests for "A Simplistic TCP Finite State Machine (FSM)" by oldccoder

---
+++ Kata/Kyu4/ASImplisticTCPFiniteStateMachine.cs
@@ -0,0 +1,63 @@
+﻿//
+// This file is part of Stefan Haglund's CodeWars Kata Solutions Collection (SHCKSC),
+// written and maintained by Stefan Haglund (stefan.haglund@me.com).
+//
+// SHCKSC is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// SHCKSC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.
+// If not, see <https://www.gnu.org/licenses/>.
+//
+//-------------------------------------------------------------------------------------------------
+
+
+// Kata: "A Simplistic TCP Finite State Machine (FSM)" by oldccoder
+// SOLUTION
+
+
+using Kata;
+using Kata.Kyu4;
+
+
+namespace Kata.Kyu4
+{
+    public class ASImplisticTCPFiniteStateMachine
+    {
+        private static Dictionary<string,Dictionary<string,string>> transitionFromBy=new() {
+            ["CLOSED"]      = new() { ["APP_PASSIVE_OPEN"] = "LISTEN",
+                                      ["APP_ACTIVE_OPEN"]  = "SYN_SENT" },
+            ["LISTEN"]      = new() { ["RCV_SYN"]          = "SYN_RCVD",
+                                      ["APP_SEND"]         = "SYN_SENT",
+                                      ["APP_CLOSE"]        = "CLOSED" },
+            ["SYN_RCVD"]    = new() { ["APP_CLOSE"]        = "FIN_WAIT_1",
+                                      ["RCV_ACK"]          = "ESTABLISHED" },
+            ["SYN_SENT"]    = new() { ["RCV_SYN"]          = "SYN_RCVD",
+                                      ["RCV_SYN_ACK"]      = "ESTABLISHED",
+                                      ["APP_CLOSE"]        = "CLOSED" },
+            ["ESTABLISHED"] = new() { ["APP_CLOSE"]        = "FIN_WAIT_1",
+                                      ["RCV_FIN"]          = "CLOSE_WAIT" },
+            ["FIN_WAIT_1"]  = new() { ["RCV_FIN"]          = "CLOSING",
+                                      ["RCV_FIN_ACK"]      = "TIME_WAIT",
+                                      ["RCV_ACK"]          = "FIN_WAIT_2" },
+            ["CLOSING"]     = new() { ["RCV_ACK"]          = "TIME_WAIT" },
+            ["FIN_WAIT_2"]  = new() { ["RCV_FIN"]          = "TIME_WAIT" },
+            ["TIME_WAIT"]   = new() { ["APP_TIMEOUT"]      = "CLOSED" },
+            ["CLOSE_WAIT"]  = new() { ["APP_CLOSE"]        = "LAST_ACK" },
+            ["LAST_ACK"]    = new() { ["RCV_ACK"]          = "CLOSED" }
+        };
+        
+        public static string TraverseStates(string[] events) {
+            string state="CLOSED"; // initial state
+
+            try {
+                foreach(string evt in events) { state=transitionFromBy[state][evt]; }
+                return state;
+            } catch(KeyNotFoundException) { return "ERROR"; }
+        }
+    }
+}
---
+++ Tests/Kyu4/ASImplisticTCPFiniteStateMachine.cs
@@ -0,0 +1,68 @@
+﻿//
+// This file is part of Stefan Haglund's CodeWars Kata Solutions Collection (SHCKSC),
+// written and maintained by Stefan Haglund (stefan.haglund@me.com).
+//
+// SHCKSC is free software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// SHCKSC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with this program.
+// If not, see <https://www.gnu.org/licenses/>.
+//
+//-------------------------------------------------------------------------------------------------
+
+
+// Kata: "A Simplistic TCP Finite State Machine (FSM)" by oldccoder
+// UNIT TESTS
+
+
+using Kata;
+using Kata.Kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Tests.Kyu4
+{
+    [TestClass]
+    public class ASImplisticTCPFiniteStateMachine
+    {
+        [TestMethod]
+        public void Test1() {
+            string correct="CLOSE_WAIT";
+            string result=Kata.Kyu4.ASImplisticTCPFiniteStateMachine.TraverseStates(["APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN"]);
+            AssertionHelpers.AssertStringsEqual(correct, result);
+        }
+
+        [TestMethod]
+        public void Test2() {
+            string correct="ESTABLISHED";
+            string result=Kata.Kyu4.ASImplisticTCPFiniteStateMachine.TraverseStates(["APP_PASSIVE_OPEN", "RCV_SYN", "RCV_ACK"]);
+            AssertionHelpers.AssertStringsEqual(correct, result);
+        }
+
+        [TestMethod]
+        public void Test3() {
+            string correct="LAST_ACK";
+            string result=Kata.Kyu4.ASImplisticTCPFiniteStateMachine.TraverseStates(["APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN", "APP_CLOSE"]);
+            AssertionHelpers.AssertStringsEqual(correct, result);
+        }
+
+        [TestMethod]
+        public void Test4() {
+            string correct="SYN_SENT";
+            string result=Kata.Kyu4.ASImplisticTCPFiniteStateMachine.TraverseStates(["APP_ACTIVE_OPEN"]);
+            AssertionHelpers.AssertStringsEqual(correct, result);
+        }
+
+        [TestMethod]
+        public void Test5() {
+            string correct="ERROR";
+            string result=Kata.Kyu4.ASImplisticTCPFiniteStateMachine.TraverseStates(["RCV_SYN", "RCV_ACK", "APP_CLOSE", "APP_SEND"]);
+            AssertionHelpers.AssertStringsEqual(correct, result);
+        }
+    }
+}
